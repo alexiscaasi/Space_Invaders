3ecf31e36a2e0b8c7a62fd39751b8c61d4f89535
Alexis Caasi <42218961+alexiscaasi@users.noreply.github.com>
2021-03-14 23:31:13 -0700

    Add files via upload

---
+++ Scripts/Bullet.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+  private Transform bullet;
+  public float speed;
+
+  // Use this for initialization
+  void Start() {
+    bullet = GetComponent<Transform> ();
+  }
+
+  void FixedUpdate() {
+    bullet.position += Vector3.up * speed;
+
+    if(bullet.position.y >= 6) {
+      Destroy(gameObject);
+    }
+  }
+
+  void OnTriggerEnter2D(Collider2D other) {
+    // if(other.tag == "Enemy") {
+    //   Destroy(other.gameObject);
+    //   Destroy(gameObject);
+    //   Score.playerScore += 10;
+    // }
+    if(other.tag == "FirstEnemy") {
+      Destroy(other.gameObject);
+      Destroy(gameObject);
+      Score.playerScore += 10;
+    }
+    if(other.tag == "SecondEnemy") {
+      Destroy(other.gameObject);
+      Destroy(gameObject);
+      Score.playerScore += 20;
+    }
+    if(other.tag == "ThirdEnemy") {
+      Destroy(other.gameObject);
+      Destroy(gameObject);
+      Score.playerScore += 30;
+    }
+    if(other.tag == "FourthEnemy") {
+      Destroy(other.gameObject);
+      Destroy(gameObject);
+      Score.playerScore += 40;
+    }
+    else if(other.tag == "Base") {
+      Destroy(gameObject);
+    }
+  }
+}
---
+++ Scripts/EnemyBullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+
+    private Transform bullet;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullet = GetComponent<Transform> ();
+    }
+
+    void FixedUpdate() {
+        bullet.position += Vector3.up * -speed;
+
+        if(bullet.position.y <= -6) {
+            Destroy(bullet.gameObject);
+        }
+    }
+
+    void OnTriggerEnter20(Collider2D other) {
+        if(other.tag == "Player") {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+            GameOver.isPlayerDead = true;
+        }
+        else if(other.tag == "Base") {
+            GameObject playerBase = other.gameObject;
+            BaseHealth baseHealth = playerBase.GetComponent<BaseHealth> ();
+            baseHealth.health -= 1;
+            Destroy(gameObject);
+        }
+    }
+}
